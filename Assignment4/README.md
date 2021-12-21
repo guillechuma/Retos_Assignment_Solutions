a43b23e929e639dc117ee7bb0e365002339b5e8f
guillechuma <guillechuma24@gmail.com>
2021-12-21 03:33:17 +0100

    Finished assignment 4

--- Assignment4/README.md
+++ Assignment4/README.md
@@ -4,7 +4,7 @@ All the code was written by me (Guillermo Chumaceiro).
 
 This program does a reciprocal best blast hits (RBH) between all proteins of Arabidopsis thaliana and Schizosaccharomyces pombe to find putative orthologues between the two species.
 
-An important remark in finding orthologues through reciprocal best hits is that comparing protein sequences give more accurate results, since they are less redundant than nucleic acids. That is the reason why I chose to convert all the sequences to protein and create a protein database to find the orthologues.
+An important remark in finding orthologues through reciprocal best hits is that comparing protein sequences give more accurate results, since they are less redundant than nucleic acids. That is the reason why I chose to convert all the sequences to protein and create a protein database to find the orthologues. Also, performing blastp is more efficient than tblastx.
 
 I adjusted two parameters of the BLASTp program to find orthologues [1]:
 
@@ -13,15 +13,23 @@ I adjusted two parameters of the BLASTp program to find orthologues [1]:
 
 To run the program you need to clone this repository and use the command line. You need to have blast and blast-legacy installed in you local computer. The main file is named [main.rb](./main.rb). The command to run the program is: 
 ```
-ruby main.rb ./data/athaliana.fa ./data/spombe.fa
+ruby main.rb ./data/athaliana.fa ./data/spombe.fa RBH_arabidopsis_spombe.txt
 ```
 
 The result of the program is a report that can be found here: [RBH_ara_spombe.txt](./RBH_ara_spombe.txt)
 
-The next step to determine the homologous proteins between these two species would be to make a phylogenetic analysis on the putative orthologues obtained in the Reciprocal Best Hit. A phylogenetic tree gives a more accurate representation of the evolutionary history between the two species, thus allowing to differentiate paralogues from orthologues. Another thing that phylogenetic trees captures are co-orthologs.
+### Next steps to confirm orthology
 
-Bibliography
+1. The ideal step to determine the orthologue proteins between these two species would be to make a phylogenetic analysis on the putative orthologues obtained in the Reciprocal Best Hit. A phylogenetic tree gives a more accurate representation of the evolutionary history between the two species, thus allowing to differentiate paralogues from orthologues. Another thing that phylogenetic trees captures are co-orthologs. The idea behind this method, although it is computationally more expensive, would be to include a third species (the more the better) and find putative orthologues and construct a gene tree with the ortologue genes. then we find the species tree between and reconcile both trees to find the duplication and speciation events, thus separating orthologues from paralogues. This step is hard and computationally expensive [2].
+
+2. Another way we can include a third species to the analysis of orthology is to do RBH of the third species with the initial two and form what are called Clusters of Orthologous Groups (COG). If two genes are the RBH, then if we compare them both to a third gene and are at the same time RBH of that third gene, chances are high that these genes are indeed orthologues. To form a COG we need that the RBH of the three species form a triangle relationship [3]. 
+
+3. Another easier and valid approach that we can do to confirm that two RBH are orthologues is to make a functional analysis of the pair of genes. In theory, orhologues are more likely to have the same function. So a simple way to eliminate false positives is to find the GO terms of each pair and see if they are the same, specially the molecular function and the biological process. We can determine a threshold to consider two genes homologues. For example, if they same more than 80% of their GO terms they are orthologues [2].
+
+### Bibliography
 
 [1] Ward, N., & Moreno-Hagelsieb, G. (2014). Quickly finding orthologs as reciprocal best hits with BLAT, LAST, and UBLAST: how much do we miss?. PloS one, 9(7), e101850. https://doi.org/10.1371/journal.pone.0101850
 
-[2]
+[2] Fang, G., Bhardwaj, N., Robilotto, R., & Gerstein, M. B. (2010). Getting started in gene orthology and functional analysis. PLoS computational biology, 6(3), e1000703. https://doi.org/10.1371/journal.pcbi.1000703
+
+[3] Li, L., Stoeckert, C. J., Jr, & Roos, D. S. (2003). OrthoMCL: identification of ortholog groups for eukaryotic genomes. Genome research, 13(9), 2178–2189. https://doi.org/10.1101/gr.1224503
--- Assignment4/main.rb
+++ Assignment4/main.rb
@@ -4,20 +4,22 @@ require 'bio'
 input_array = ARGV
 
 # Check if CL arguments are two 
-if input_array.empty? or (input_array.length != 2) then 
+if input_array.empty? or (input_array.length != 3) then 
 	$ARA_FILE = "./data/athaliana.fa"
   $SPOMBE_FILE = "./data/spombe.fa"
+  $REPORT_FILE = "RBH_arabidopsis_spombe.txt"
 else
 	# Case where there are command line arguments
 	$ARA_FILE = input_array[0]
   $SPOMBE_FILE = input_array[1]
+  $REPORT_FILE = input_array[2]
 end
 
 # Convert A. Thaliana nucleotide sequences to protein fasta. It is more accurate to do blast between proteins.
 # Also, blastp is more efficient than tblastx in comparing sequences.
 
 faaoutput = File.open('./data/athaliana.faa', 'w') # Name of the output file
-fasta_file = Bio::FlatFile.auto("./data/athaliana.fa")
+fasta_file = Bio::FlatFile.auto($ARA_FILE)
 fasta_file.each_entry do |x| # Go though each fasta record
   # If translation includes codon stop as *, do not include it.
   if x.naseq.translate[-1] == "*" 
@@ -41,6 +43,8 @@ puts "Database for S.pombe created" if system("makeblastdb -in ./data/spombe.fa 
 # Auxiliary method to index the fasta files
 # This method creates a dictionary of the fasta file with the header of each record as key and the complete fasta as value.
 # This mantains the sequences in memory and it is easier to seach than loading the file (It will be done many times).
+
+# @return [Hash<String, String>]. The dictionary with the indexed fasta files.
 def fasta2dict(fasta_file)
   fasta_dict = Hash.new
   fasta_data = Bio::FlatFile.auto(fasta_file) # Read file as Fasta Format
@@ -50,100 +54,116 @@ def fasta2dict(fasta_file)
   return fasta_dict
 end
 
-# Dictionaries for both species
-spombe_fasta = fasta2dict("./data/spombe.fa")
-athaliana_fasta = fasta2dict("./data/athaliana.faa")
-
 # ---------------------------------------------
 # Perform Reciprocal Blast Hit
 # ---------------------------------------------
 
 # blast and blast-legacy must be installed globally
 
-# Create a blastp factory to search the athaliana protein database with an E-value threshold o 1E-6
-# Matches using proteins are more biologically meaningfull!!
-factory = Bio::Blast.local('blastp', './databases/athaliana_prot', '-e 0.000001')
+# Perform a Reciprocal best hit between the proteome of two species using blastp to query their databases.
+# The result is a report of the putative orthologues between the two species.
+# 
+# @param proteome_1 [String] The file to the proteome of the first species.
+# @param proteome_2 [String] The file to the proteome of the second species.
+# @param database_1 [String] The blast database of the first species.
+# @param database_2 [String] The blast database of the second species.
+# @param report_name [String] The name of the final report.
+def RBH(proteome_1, proteome_2, database_1, database_2, report_name)
 
-# Use the genome of S. Pombe as query
-query_file = "./data/spombe.fa"
-query_data = Bio::FlatFile.auto(query_file) # Fetch fasta as bioruby fasta:Format
+  # Create a blastp factory to search the athaliana protein database with an E-value threshold o 1E-6
+  # Matches using proteins are more biologically meaningfull!!
+  factory = Bio::Blast.local('blastp', database_1, '-e 0.000001')
 
-# Create a blastp factory to search the S.pombe protein database with an E-value threshold o 1E-6 
-# This is the reciprocal database
-reciprocal_factory = Bio::Blast.local('blastp', './databases/spombe', '-e 0.000001')
+  # Use the genome of S. Pombe as query
+  query_file = proteome_2
+  query_data = Bio::FlatFile.auto(query_file) # Fetch fasta as bioruby fasta:Format
 
-# REGEX to extract only the ID from the header of each fasta record. This is used to compare matches.
-id_regex = Regexp.new(/^(\w+\.\w+)/i)
+  # Dictionaries for A. thaliana
+  athaliana_fasta = fasta2dict(proteome_1)
 
-# Keep track of the number of putative orthologues
-ort_count = 0
+  # Create a blastp factory to search the S.pombe protein database with an E-value threshold o 1E-6 
+  # This is the reciprocal database
+  reciprocal_factory = Bio::Blast.local('blastp', database_2, '-e 0.000001')
 
-# Open the report file
-File.open('RBH_ara_spombe.txt','w') do |file|
+  # REGEX to extract only the ID from the header of each fasta record. This is used to compare matches.
+  id_regex = Regexp.new(/^(\w+\.\w+)/i)
 
-  # Header of the report
-  file.write("Orthologues between A.thaniana and S.pombe\n")
-  file.write("S.pombe gene\tA.thaniana gene\n")
+  # Keep track of the number of putative orthologues
+  ort_count = 0
 
-  # Iterate though all the query fasta files
-  query_data.each_entry do |query_fasta|
+  puts "Searching for Reciprocal Best Hits"
 
-    query = query_fasta.to_s # Convert to string (Input to factory)
-    report = factory.query(query) # Bio::Blast::Report
-    # The BRH is the BEST hit, get only the first one.
-    hit = report.hits[0] # hit: Bio::Blast::Report::Hit
-    next if hit.nil? # Filter when there are no hits
-    # From the best hit, get the best HSP
-    hsp = hit.hsps[0] # Bio::Blast::Report::Hsp
-    next if hsp.nil? # filter when there are no HSP
+  # Open the report file
+  File.open(report_name,'w') do |file|
 
-    # Calculate the coverage as alignment length/Query length
-    cov = hsp.align_len/query_fasta.length.to_f
+    # Header of the report
+    file.write("Orthologues between A.thaniana and S.pombe\n")
+    file.write("S.pombe gene\tA.thaniana gene\n")
 
-    # Find the query ID using regex expression
-    if query_fasta.definition.match(id_regex)
-      query_id = $1
-    end
+    # Iterate though all the query fasta files
+    query_data.each_entry do |query_fasta|
 
-    # Sensitivity filter. Coverage must be higher or equal to 50%
-    if cov >= 0.5
-      # Make blast with other seq
-      # Get the sequence from the BEST HIT, it will be the reciprocal query
-      reciprocal_query = Bio::FastaFormat.new(athaliana_fasta[hit.target_def])
-
-      # Retrieve it's ID
-      reciprocal_query_id = reciprocal_query.entry_id
-
-      # Search the reciprocal query in the  S. pombe database
-      reciprocal_report = reciprocal_factory.query(reciprocal_query.to_s) # Bio::Blast::Report
-
+      query = query_fasta.to_s # Convert to string (Input to factory)
+      report = factory.query(query) # Bio::Blast::Report
       # The BRH is the BEST hit, get only the first one.
-      reciprocal_hit = reciprocal_report.hits[0] # hit: Bio::Blast::Report::Hit
-      next if reciprocal_hit.nil? # Filter no hits
+      hit = report.hits[0] # hit: Bio::Blast::Report::Hit
+      next if hit.nil? # Filter when there are no hits
       # From the best hit, get the best HSP
-      reciprocal_hsp = reciprocal_hit.hsps[0] # Bio::Blast::Report::Hsp
-      next if reciprocal_hsp.nil? # filter when there are no HSP
+      hsp = hit.hsps[0] # Bio::Blast::Report::Hsp
+      next if hsp.nil? # filter when there are no HSP
 
-      # Get the reciprocal target ID with the regex expression
-      if reciprocal_hit.target_def.match(id_regex)
-        reciprocal_target_id = $1
+      # Calculate the coverage as alignment length/Query length
+      cov = (hit.query_end.to_f - hit.query_start.to_f + 1) / query_fasta.length.to_f
+
+      # Find the query ID using regex expression
+      if query_fasta.definition.match(id_regex)
+        query_id = $1
       end
 
-      # Calculate reciprocal coverage
-      reciprocal_cov = reciprocal_hsp.align_len/reciprocal_query.length.to_f
-      
-      # Filter by coverage higher or equal to 50%
-      if reciprocal_cov >= 0.5
-        # Check if the target hit is the query hit of the first blast
-        if reciprocal_target_id == query_id
-          # That means that the query and the reciprocal are putative ORTHOLOGUES.
-          file.write("#{query_id}\t#{reciprocal_query_id}\n") # Write line to report
-          ort_count += 1
+      # Sensitivity filter. Coverage must be higher or equal to 50%
+      if cov >= 0.5
+        # Make blast with other seq
+        # Get the sequence from the BEST HIT, it will be the reciprocal query
+        reciprocal_query = Bio::FastaFormat.new(athaliana_fasta[hit.target_def])
+
+        # Retrieve it's ID
+        reciprocal_query_id = reciprocal_query.entry_id
+
+        # Search the reciprocal query in the  S. pombe database
+        reciprocal_report = reciprocal_factory.query(reciprocal_query.to_s) # Bio::Blast::Report
+
+        # The BRH is the BEST hit, get only the first one.
+        reciprocal_hit = reciprocal_report.hits[0] # hit: Bio::Blast::Report::Hit
+        next if reciprocal_hit.nil? # Filter no hits
+        # From the best hit, get the best HSP
+        reciprocal_hsp = reciprocal_hit.hsps[0] # Bio::Blast::Report::Hsp
+        next if reciprocal_hsp.nil? # filter when there are no HSP
+
+        # Get the reciprocal target ID with the regex expression
+        if reciprocal_hit.target_def.match(id_regex)
+          reciprocal_target_id = $1
+        end
+
+        # Calculate reciprocal coverage
+        reciprocal_cov = (reciprocal_hit.query_end.to_f - reciprocal_hit.query_start.to_f + 1) / reciprocal_query.length.to_f
+        
+        # Filter by coverage higher or equal to 50%
+        if reciprocal_cov >= 0.5
+          # Check if the target hit is the query hit of the first blast
+          if reciprocal_target_id == query_id
+            # That means that the query and the reciprocal are putative ORTHOLOGUES.
+            file.write("#{query_id}\t#{reciprocal_query_id}\n") # Write line to report
+            ort_count += 1 # Keep track of the number of orthologues
+          end
         end
       end
     end
   end
+
+  puts "Found #{ort_count} orthologues between S. pombe and A. thaliana"
 end
 
-puts "Found #{ort_count} orthologues between S. pombe and A. thaliana"
+# Run Reciprocal Best Hit program with both proteomes
+RBH("./data/athaliana.faa", "./data/spombe.fa", './databases/athaliana_prot', './databases/spombe', $REPORT_FILE)
+
 puts "Program ended"
